2c45e77f98cd8878ae52a7c2907fc739ac388c4c
Md Abu Naser Kayes <abunaserkayes0@gmail.com>
2022-06-25 16:16:24 +0600

    implement top-categories

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,7 @@
 import Banner from '../Banner/Banner';
 import Inventories from '../Inventories/Inventories';
 import Reviews from '../Reviews/Reviews';
+import TopCategories from '../TopCategories/TopCategories';
 import './Home.css';
 const Home = () => {
     return (
@@ -8,6 +9,7 @@ const Home = () => {
             <Banner></Banner>
             <Inventories></Inventories>
             <Reviews></Reviews>
+            <TopCategories></TopCategories>
         </div>
     );
 };
